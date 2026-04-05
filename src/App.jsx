5d15ef7e398a5560a7a6b7cb089301c86e67831a
Taras1326 <166243411+Taras1326@users.noreply.github.com>
2026-04-05 10:34:36 +0300

    rasss

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import recipies from "./recipies.json"
-import { IoMdTime } from "react-icons/io";
-import { CiPizza } from "react-icons/ci";
-import { MdOutlineLunchDining, MdDriveFileRenameOutline } from "react-icons/md";
-import { GiNetworkBars } from "react-icons/gi";
+
+import Recipe from "./components/Recipe/Recipe"
 import './App.css'
 
 function App() {
-console.log(recipies)
+
 
   return (
     <>
-    <ul>{recipies.map(({name, time, servings, calories, difficulty, image})=>{
-      return(
-        <li key={name}>
-          <p><MdDriveFileRenameOutline />{name}</p>
-          <p><IoMdTime />{time}</p>
-          <p><CiPizza />{servings}</p>
-          <p><MdOutlineLunchDining />{calories}</p>
-          <p><GiNetworkBars />{difficulty}</p>
-          <img src={image} alt={name} width={300} />
-        </li>
-      )
-    })}</ul>
+    <Recipe recipies={recipies}/>
     </>
   )
 }
---
+++ src/components/Recipe/Recipe.jsx
@@ -0,0 +1,24 @@
+
+import { IoMdTime } from "react-icons/io";
+import { CiPizza } from "react-icons/ci";
+import { MdOutlineLunchDining, MdDriveFileRenameOutline } from "react-icons/md";
+import { GiNetworkBars } from "react-icons/gi";
+
+function Recipe({recipies}) {
+    return (
+        <ul>{recipies.map(({name, time, servings, calories, difficulty, image})=>{
+            return(
+              <li key={name}>
+                <p><MdDriveFileRenameOutline />{name}</p>
+                <p><IoMdTime />{time}</p>
+                <p><CiPizza />{servings}</p>
+                <p><MdOutlineLunchDining />{calories}</p>
+                <p><GiNetworkBars />{difficulty}</p>
+                <img src={image} alt={name} width={300} />
+              </li>
+            )
+          })}</ul>
+    )
+}
+
+export default Recipe
